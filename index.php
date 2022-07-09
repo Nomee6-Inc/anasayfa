a48a6581f5d393830af660531fa1521d4f17940a
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-07-09 23:09:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -96,13 +96,6 @@ if (isset($_POST['submit'])) {
       xhr.send();
   });
 </script>
-<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "cp7t4rmvc6");
-</script>
 </head>
 <body>
 <header>
